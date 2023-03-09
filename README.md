ecc865bf9fada8f514bc2bd1044ce01b35852f4e
Sean David Abalora <seanabalora@gmail.com>
2023-03-10 07:55:28 +0800

    Fixed display bugs and added comments

--- README.md
+++ README.md
@@ -3,9 +3,6 @@
 Control your ninja to go to four different places to get gold. Once you reach 250 gold, you win! If the timer runs out and you don't have 250 gold, you lose!
 
 # Versions
-v1.0 - Added skeleton/base UI. Added functions for the Cave, House, Gold Mine, and Casino locations.
-v1.1 - Cleaned the skeleton/base UI. 
-
-
-# Known Bugs
-v1.0 - The gold in the gold counter gets overwritten in the Gold Mine and Casino locations.
+v1.0 - Added skeleton/base UI. Added functions for the Cave, House, Gold Mine, and Casino locations. <br />
+v1.1 - Cleaned the skeleton/base UI. <br />
+v1.2 - Fixed Gold Mine and Casino display bugs and added comments.
--- js/script.js
+++ js/script.js
@@ -1,103 +1,167 @@
 var gameState = {
     attempts: 20,
     gold: 0,
-    events: ["cave", "house", "gold mine", "casino"]
+    log: []
 }
 
-var totalAssets = {
+var totalGold = {
     gained: 0,
     lost: 0
 }
 
+let randomizer = 0;
+
+//Cave Function (100% chance. 5 Gold.)
 $("#cave").click(cave);
 
 function cave() {
+    //-1 Attempt
     gameState.attempts--;
     $("#attempts").html(gameState.attempts);
 
+    //Adds 5 gold to total earnings
+    totalGold.gained += 5;
+    $("#gained").html(totalGold.gained);
+
+    //Adds 5 Gold to overall gold
     gameState.gold += 5;
     $("#gold").html(gameState.gold);
 
-    $("#log").html("You earned 5 gold coins!");
-    console.log("You have earned 5 coins!");
+    //Logs the Action
+    console.log("%cYou have earned 5 coins!", 'color: green');
+    $("#log").html("You have earned 5 coins!");
+    $("#log").css('color', 'green');
 
     gameOver();
 }
 
+//House Function (80% chance. 10 Gold.)
 $("#house").click(house);
 
 function house() {
+    //-1 Attempt
     gameState.attempts--;
     $("#attempts").html(gameState.attempts);
 
-    if (Math.random() <= 0.8) {
-        gameState.gold += 10;
-        $("#log").html("You earned 10 gold coins!");
-        console.log("You earned 10 coins!");
-    } else {
-        gameState.gold += 0;
-        $("#log").html("You earned 0 gold coins!");
-        console.log("You earned 0 coins!");
-    }
+    //Probability of getting gold
+    randomizer = Math.floor(Math.random() * 100);
+    houseProbability = 80;
 
-    $("#gold").html(gameState.gold);
+    //Math.random() generates a number between 1 and 2
+    if (randomizer <= houseProbability) {
+        //Adds 10 gold to overall gold
+        gameState.gold += 10;
+        $("#gold").html(gameState.gold);
+
+        //Adds 10 gold total earnings
+        totalGold.gained += 10;
+        $("#gained").html(totalGold.gained);
+
+        //Logs the Action
+        $("#log").html("You earned 10 gold coins!");
+        console.log("%cYou have earned 10 coins!", 'color: green');
+    } else {
+        //Adds 0 gold to overall gold
+        gameState.gold += 0;
+        $("#gold").html(gameState.gold);
+
+        //Logs the Action
+        console.log("%cYou have earned 0 coins!", 'color: green');
+        $("#log").html("You earned 0 gold coins!");
+        $("#log").css('color', 'green');
+    }
 
     gameOver();
 }
 
+//Gold Mine Function (80% chance. Between 1 to 25 Gold.)
 $("#goldMine").click(goldMine);
 
-function goldMineRandomizer(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-var goldMineNumber = goldMineRandomizer(1, 25);
+let randomMineGold = 0;
 
 function goldMine() {
+    //-1 Attempt
     gameState.attempts--;
     $("#attempts").html(gameState.attempts);
 
-    if (Math.random() <= 0.8) {
-        gameState.gold += goldMineNumber;
+    //Probability of getting gold
+    randomizer = Math.floor(Math.random() * 100);
+    mineProbability = 80;
+
+    if (randomizer <= mineProbability) {
+        //Generates between 1 to 25 gold
+        randomMineGold = Math.floor(Math.random() * 25);
+
+        //Adds randomzied gold to overall gold
+        gameState.gold += randomMineGold;
         $("#gold").html(gameState.gold);
-        $("#log").html("You earned " + gameState.gold + " coins");
-        console.log("You have earned %d coins!", gameState.gold);
+
+        //Adds randomized gold to total earnings
+        totalGold.gained += randomMineGold;
+        $("#gained").html(totalGold.gained);
+
+        //Logs the action
+        console.log("%cYou have earned %d coins!", 'color: green', randomMineGold);
+        $("#log").html("You earned " + randomMineGold + " coins");
+        $("#log").css('color', 'green');
     } else {
         gameState.gold += 0;
+        console.log("%cYou earned 0 coins!", 'color: green');
         $("#log").html("You earned 0 gold coins!");
-        console.log("You earned 0 coins!");
+        $("#log").css('color', 'green');
     }
-
     gameOver();
 }
 
+//Casino Function (50% chance. Gain or Lose Between 40 to 50 Gold.)
 $("#casino").click(casino);
 
 function casino() {
+    //-1 Attempt
     gameState.attempts--;
     $("#attempts").html(gameState.attempts);
 
-    if (Math.random() <= 0.5) {
-        gameState.gold += Math.floor(Math.random() * (50 - 40) + 40);
-        $("#log").html("You earned " + gameState.gold + " coins");
-        console.log("You earned %d coins!", gameState.gold);
-        $("#gold").html(gameState.gold);
-    } else {
-        gameState.gold += Math.floor(Math.random() * (50 - 40) + 40);
-        $("#log").html("You lost " + gameState.gold + " coins");
-        console.log("You lost %d coins!", gameState.gold);
-        $("#gold").html(gameState.gold);
-    }
+    //Probability of getting/losing gold
+    randomizer = Math.floor(Math.random() * 100);
+    casinoProbability = 50;
 
+    if (randomizer <= casinoProbability) {
+        //Generates between 40 to 50 gold
+        randomCasinoGold = Math.floor(Math.random() * (50 - 40) + 40);
+
+        //Adds randomzied gold to overall gold
+        gameState.gold += randomCasinoGold;
+        $("#gold").html(gameState.gold);
+
+        //Adds randomized gold to total earnings
+        totalGold.gained += randomCasinoGold;
+        $("#gained").html(totalGold.gained);
+
+        //Logs the action
+        console.log("%cYou have earned %d coins!", 'color: green', randomCasinoGold);
+        $("#log").html("You earned " + randomCasinoGold + " coins");
+        $("#log").css('color', 'green');
+    } else {
+        //Generates between 40 to 50 gold
+        randomCasinoGold = Math.floor(Math.random() * (50 - 40) + 40);
+
+        //Deducts randomzied gold to overall gold
+        gameState.gold -= randomCasinoGold;
+        $("#gold").html(gameState.gold);
+
+        //Adds the randomized gold to total losses
+        totalGold.lost += randomCasinoGold;
+        $("#lost").html(totalGold.lost);
+
+        //Logs the action
+        console.log("%cYou lost %d coins!", 'color: red', randomCasinoGold);
+        $("#log").html("You lost " + randomCasinoGold + " coins");
+        $("#log").css('color', 'red');
+    }
     gameOver();
 }
 
-$("#reset").click(reset);
-
-function reset() {
-    location.reload();
-}
-
+//Alerts the end of the Game
 function gameOver() {
     if (gameState.gold >= 250) {
         alert("You Win!");
@@ -110,4 +174,11 @@ function gameOver() {
         alert("You Win!");
         location.reload();
     }
+}
+
+//Resets the game
+$("#reset").click(reset);
+
+function reset() {
+    location.reload();
 }
